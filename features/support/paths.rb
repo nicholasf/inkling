39264896366c85d479d50380c384eb32eee0bc20
Nicholas Faiz <nicholas@andromeda.local>
2010-05-21 13:54:35 +1000

    fixed the one cucumber feature

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -11,7 +11,7 @@ module NavigationHelpers
     when /the home\s?page/
       '/'
     when /admin/
-      'admin/inkling'
+      '/admin/inkling'
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:

769d272a5b70eaa446294374343b4f1b635baa20
biv <nicholas.faiz@gmail.com>
2010-08-18 23:53:22 +1000

    exp. with overriding views

--- config/initializers/override_devise_after_sign_in.rb
+++ config/initializers/override_devise_after_sign_in.rb
@@ -1,15 +1,18 @@
-module Devise
-  module Controllers
-    module Helpers
-      
-      alias :old_after_sign_in_path_for :after_sign_in_path_for
-      
-      def after_sign_in_path_for(resource_or_scope)
-        scope = Devise::Mapping.find_scope!(resource_or_scope)
-        role_name = resource_or_scope.roles.first.name
-        home_path = :"#{role_name}_root_path"
-        respond_to?(home_path, true) ? send(home_path) : root_path
-      end
-    end
-  end
-end
+# module Devise
+#   module Controllers
+#     module Helpers
+#       
+#       alias :old_after_sign_in_path_for :after_sign_in_path_for
+#       
+#       def after_sign_in_path_for(resource_or_scope)
+#         scope = Devise::Mapping.find_scope!(resource_or_scope)
+#         home_path = :"#{scope}_root_path"
+#         unless resource_or_scope.is_a? Symbol
+#           role_name = resource_or_scope.roles.first.name 
+#           home_path = :"#{role_name}_root_path"
+#         end
+#         respond_to?(home_path, true) ? send(home_path) : root_path
+#       end
+#     end
+#   end
+# end

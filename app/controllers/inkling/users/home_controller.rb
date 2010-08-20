6c485fc1be7d1c823687ae5557a6987898f0f995
biv <nicholas.faiz@gmail.com>
2010-08-20 14:06:17 +1000

    use helper methods for loading partials from file system

--- app/controllers/inkling/users/home_controller.rb
+++ app/controllers/inkling/users/home_controller.rb
@@ -1,3 +1,35 @@
 class Inkling::Users::HomeController < Inkling::BaseController
+  helper_method :dashboard_partials
+  
+  
+  
+  #protected
+  #these methods would be better in a helper, but it seems helpers have issues loading from engines.
+  def render_dashboard_partials
+    for file in dashboard_partials
+    	render(:file => file)
+  	end
+  end
+  
+  
+  def dashboard_partials
+    partials = []
 
+    for engine in Rails.application.railties.engines  
+      for path in engine.paths.all_paths
+        path_str = path.first    
+        if path_str =~ /views/
+    	    if File.exist?("#{path_str}/inkling/users/home/")
+    		    for entry in Dir.entries("#{path_str}/inkling/users/home/")
+    			    if entry =~ /_dashboard\./
+    				    partials << "#{path_str}/inkling/users/home/#{entry}"		
+    			    end
+    		    end
+    	    end
+        end    
+      end
+    end
+
+    partials
+  end
 end
--- app/helpers/inkling/users/home/home_helper.rb
+++
@@ -1,4 +0,0 @@
-module Inkling::Users::HomeHelper
-  def render_dashboard_partials
-  end
-end

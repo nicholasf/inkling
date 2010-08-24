3724ab9c9e460b296ab501307e4acfd3f66c6af5
biv <nicholas.faiz@gmail.com>
2010-08-24 16:36:06 +1000

    upgrading to rails 3 rc 2, and introducing one helluva bug

--- lib/inkling/routing.rb
+++ lib/inkling/routing.rb
@@ -8,12 +8,14 @@ module Inkling
       end
 
       def matches?(request)
+        puts "*** searching to match path"
         path = request.path.gsub("/inkling", "")
         matching_path = Inkling::Path.find_by_path(path)
 
         if matching_path
           result = matching_path.content.is_a? @type
           request.params[:id] = matching_path.content.id
+          puts "*** found and located matching content for #{@type} - #{request.params}"
           result
         else
           false

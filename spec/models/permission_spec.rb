ec3f7735c69a6039669a6bb67bbe46f1c2f8c10d
Nicholas Faiz <nicholas.faiz@gmail.com>
2010-09-06 17:50:10 +1000

    witness the foolishness of the foos migration

--- spec/models/permission_spec.rb
+++ spec/models/permission_spec.rb
@@ -1,17 +1,32 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Inkling::Permission do
-
   let(:foo) {ContentTypes::Foo.create(:name => "1")}
-  let(:admin_role) {Inkling::Role.create(:name => Inkling::Role::ADMIN)}
   let(:user) {Inkling::User.create!(:email => "admin@localhost.com", :password => "test123", :password_confirmation => "test123")}  
-  let(:membership) {Inkling::RoleMembership.create!(:user => user, :role => admin_role)}
-  
 
-  it "administrator should have automatic manage permission over foo" do
-    membership;
+  it "should let an administrator have automatic manage permission over foo" do
+    # let(:admin_role) {Inkling::Role.create(:name => Inkling::Role::ADMIN)}
+    # let(:user) {Inkling::User.create!(:email => "admin@localhost.com", :password => "test123", :password_confirmation => "test123")}  
+    # let(:membership) {Inkling::RoleMembership.create!(:user => user, :role => admin_role)}
     
+    create_role_and_membership(Inkling::Role::ADMIN, user)
+
     ability = Inkling::Ability.new(user)
     ability.can?(:manage, ContentTypes::Foo).should be_true
+  end
+  
+  it "should apply CRUD based perm.s via the role to the user on the content type" do
+    create_role_and_membership("content_editor", user)
+    
+    permission = Inkling::Permission.create(:path => foo.path, :role => role, :action => :update)
+    ability = Inkling::Ability.new(user)
+    ability.can?(:update, ContentTypes::Foo).should be_true
+  end
+  
+  
+  def create_role_and_membership(role_name, user)
+    role = Inkling::Role.create(:name => role_name)
+    membership = Inkling::RoleMembership.create!(:user => user, :role => role) 
+    role
   end
 end
---
+++ testapp/db/migrate/20100906071942_create_inkling_tables.rb
@@ -0,0 +1,46 @@
+class CreateInklingTables < ActiveRecord::Migration
+  def self.up
+    create_table :inkling_users do |t|
+      t.database_authenticatable
+      t.confirmable
+      t.recoverable
+      t.rememberable
+      t.trackable
+      t.timestamps
+      t.timestamps
+    end
+    
+    create_table :inkling_roles do |t|
+      t.string :name, :null => false
+      t.timestamps
+    end
+    
+    create_table :inkling_role_memberships do |t|
+      t.integer :user_id, :null => false
+      t.integer :role_id, :null => false
+    end
+    
+    create_table :inkling_paths do |t|
+      t.integer :parent_id
+      t.integer :lft
+      t.integer :rgt
+      t.string :path
+      t.references :content, :polymorphic => true
+      t.timestamps
+    end    
+    
+    create_table :inkling_permissions do |t|
+      t.integer :path_id
+      t.integer :role_id
+      t.string :action
+      t.timestamps
+    end    
+  end
+
+  def self.down
+    drop_table :paths
+    drop_table :inkling_role_memberhips    
+    drop_table :inkling_roles
+    drop_table :inkling_users
+  end
+end

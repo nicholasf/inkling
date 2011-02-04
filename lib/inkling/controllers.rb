435aed2b8ae8ee440474fcb654d2146f38fe246c
biv <nicholas.faiz@gmail.com>
2011-02-05 06:33:12 +1100

    better method name

--- lib/inkling/controllers.rb
+++ lib/inkling/controllers.rb
@@ -6,7 +6,7 @@ module Inkling
   end
 
   module ClassMethods
-    def inkling_content
+    def inkling_content_controller
       layout 'content' #this file is created by the Theme functionality in admin, and written out to tmp/inkling/themes/layouts
       alias current_user current_inkling_user
       helper_method :current_user

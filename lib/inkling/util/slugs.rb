6c4f1954e2b7e82eeba8d51ab869a76e5ae06412
biv <nicholas.faiz@gmail.com>
2011-02-07 15:23:55 +1100

    duh duh duh, refactor the actual module namespace itself.

--- lib/inkling/util/slugs.rb
+++ lib/inkling/util/slugs.rb
@@ -1,17 +1,19 @@
 module Inkling
-  module Slugs
-    #copied from enki see http://www.enkiblog.com/  
-    def sluggerize(slug)
-      text = slug.dup
-      text.downcase!
-      text.gsub!(/&(\d)+;/, '') # Ditch Entities
-      text.gsub!('&', 'and') # Replace & with 'and'
-      text.gsub!(/['"]/, '') # replace quotes by nothing
-      text.gsub!(/\ +/, '-') # replace all white space sections with a dash
-      text.gsub!(/(-)$/, '') # trim dashes
-      text.gsub!(/^(-)/, '') # trim dashes
-      text.gsub!(/[^\/a-zA-Z0-9\-]/, '-') # Get rid of anything we don't like
-      text
+  module Util
+    module Slugs
+      #copied from enki see http://www.enkiblog.com/  
+      def sluggerize(slug)
+        text = slug.dup
+        text.downcase!
+        text.gsub!(/&(\d)+;/, '') # Ditch Entities
+        text.gsub!('&', 'and') # Replace & with 'and'
+        text.gsub!(/['"]/, '') # replace quotes by nothing
+        text.gsub!(/\ +/, '-') # replace all white space sections with a dash
+        text.gsub!(/(-)$/, '') # trim dashes
+        text.gsub!(/^(-)/, '') # trim dashes
+        text.gsub!(/[^\/a-zA-Z0-9\-]/, '-') # Get rid of anything we don't like
+        text
+      end
     end
   end
 end

cd2b0a3aec70a566b367e0cde9be90a00283c1af
biv <nicholas.faiz@gmail.com>
2011-02-08 10:47:38 +1100

    working on controller helpers again - client controllers shouldnt have to inherit from inkling controllers, should just mixin helper methods on as needed

--- config/initializers/init.rb
+++ config/initializers/init.rb
@@ -1,5 +1,5 @@
 ActiveRecord::Base.send :include, Inkling::Types::ActsAs
-# ActionController::Base.send :include, Inkling::Controllers
+ActionController::Base.send :include, Inkling::Controllers
 
 #add to view load paths for theming
 ActionController::Base.append_view_path(Inkling::THEMES_DIR)

ad6b05f8c53ff18c56999a075e37568e3ae1f9a2
biv <nicholas.faiz@gmail.com>
2011-02-23 13:31:05 +1100

    commenting out inherited resources in admin controllers for the moment, there's a class loading issue in supporting engines

--- app/controllers/inkling/admin/roles_controller.rb
+++ app/controllers/inkling/admin/roles_controller.rb
@@ -1,4 +1,4 @@
 class Inkling::Admin::RolesController < Inkling::Admin::BaseController
-  inherit_resources                                                                                     
-  defaults :resource_class => Inkling::Role, :instance_name => 'role'
+  # inherit_resources                                                                                     
+  # defaults :resource_class => Inkling::Role, :instance_name => 'role'
 end
